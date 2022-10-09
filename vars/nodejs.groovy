8e36f5b630e8b49947c975edb8b7a9ccdbdfe28e
Sudeep S <sudeepdirect@gmail.com>
2022-10-09 15:49:49 +0530

    Updated nodejs groovy file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,20 +1,3 @@
-def call() {
-pipeline {
-    agent any
-    stages {
-        // This should run for every commit of feature branch
-        stage('Lint checks') {
-            steps {
-                script {
-                    nodejs.lintcheck(COMPONENT)
-                    }
-                }
-            }
-        } //end of the stages
-    }  // end of the pipeline
-}  // end of function call
-
-// defining the lintcheck function
 def lintChecks() {
   sh '''
     echo installing jslint
@@ -23,3 +6,22 @@ def lintChecks() {
     echo lint checks completed for ${COMPONENT}
     '''
 }
+
+def call() {
+pipeline {
+    agent any
+    stages {
+        // This should run for every commit of feature branch
+        stage('Lint checks') {
+            steps {
+                script {
+                    lintchecks()
+                    }
+                }
+            }
+        } //end of the stages
+    }  // end of the pipeline
+}  // end of function call
+
+// defining the lintcheck function
+ 
