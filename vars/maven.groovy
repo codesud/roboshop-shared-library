e4ba809d9320f1e0c3678f9aa1de72439601c38c
Sudeep S <sudeepdirect@gmail.com>
2022-10-10 14:15:02 +0530

    Created maven groovy file for lintcheck

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,8 +1,7 @@
 def lintChecks() {
   sh '''
-    echo installing jslint
-    npm install jslint
-    ~/node_modules/jslint/bin/jslint.js server.js || true
+
+
     echo lint checks completed for ${COMPONENT}
     '''
 }
