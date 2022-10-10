272695b100cf9f891865b286e4406f3911d5424e
Sudeep S <sudeepdirect@gmail.com>
2022-10-10 15:24:56 +0530

    Updated pyhton groovy file for lintcheck

---
+++ vars/python.groovy
@@ -0,0 +1,27 @@
+def lintChecks() {
+  sh '''
+    echo lint checks starting for ${COMPONENT}
+    pip3 install pylint 
+    pylint *.py || true
+    echo lint checks completed for ${COMPONENT}
+    '''
+}
+
+def call() {    // call is the default function which will be called by default
+pipeline {
+    agent any
+    stages {
+        // This should run for every commit of feature branch
+        stage('Lint checks') {
+            steps {
+                script {
+                    lintChecks()
+                    }
+                }
+            }
+        } //end of the stages
+    }  // end of the pipeline
+}  // end of function call
+
+// defining the lintcheck function
+ 
