40ae5805294af94ed2d01569db9a992afc654721
Sudeep S <sudeepdirect@gmail.com>
2022-10-10 19:42:00 +0530

    Updated maven groovy file for sonarCheck

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -6,9 +6,18 @@ def lintChecks() {
     '''
 }
 
+def sonarCheck() {
+  sh '''
+    sonar-scanner -Dsonar.host.url=http://172.31.3.80:9000 -Dsonar.sources=. -Dsonar.login={SONAR_USR} -Dsonar.password={SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
+    '''
+}
+
 def call() {    // call is the default function which will be called by default
 pipeline {
     agent any
+        environment { 
+        SONAR = credentials('sonar')
+        }
     stages {
         // This should run for every commit of feature branch
         stage('Lint checks') {
