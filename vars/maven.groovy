977a6a7fffb0bf2f6a28355e53cde658df161525
Sudeep S <sudeepdirect@gmail.com>
2022-10-11 12:01:51 +0530

    Updated maven groovy file

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -12,9 +12,10 @@ def sonarCheck() {  // defining the lintcheck function
     mvn clean compile
     pwd && ls -ltr
     sonar-scanner -Dsonar.host.url=http://172.31.5.124:9000 -Dsonar.sources=. -Dsonar.login={SONAR_USR} -Dsonar.password={SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
-    curl https://raw.githubusercontent.com/CodingSudeep/lab-tools/main/sonarscanner/quality-gate.sh > quality-gate.sh 
-    quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.4.93 ${COMPONENT}
+    curl https://raw.githubusercontent.com/CodingSudeep/lab-tools/main/sonarscanner/quality-gate.sh > /tmp/quality-gate.sh 
+    chmod +x /tmp/quality-gate.sh && /tmp/quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.4.93 ${COMPONENT} 
     '''
+    // sonarscanner runs on jenkins instance.
 }
 
 def call() {    // call is the default function which will be called by default
