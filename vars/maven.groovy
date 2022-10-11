f9e983fdf6cd62c7b42d86a230ca16b6f851309a
Sudeep S <sudeepdirect@gmail.com>
2022-10-11 11:50:30 +0530

    Updated maven groovy file

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -12,6 +12,8 @@ def sonarCheck() {  // defining the lintcheck function
     mvn clean compile
     pwd && ls -ltr
     sonar-scanner -Dsonar.host.url=http://172.31.5.124:9000 -Dsonar.sources=. -Dsonar.login={SONAR_USR} -Dsonar.password={SONAR_PSW} -Dsonar.projectKey=shipping -Dsonar.java.binaries=target/classes/
+    curl https://raw.githubusercontent.com/CodingSudeep/lab-tools/main/sonarscanner/quality-gate.sh > quality-gate.sh 
+    quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.4.93 ${COMPONENT}
     '''
 }
 
