86f3c84876c068c8c92094dc348dce1117ebf47f
Sudeep S <sudeepdirect@gmail.com>
2022-10-31 08:08:17 +0530

    Updated docker groovy files

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,16 +1,17 @@
 def call() {
     node {
         sh "rm -rf *"
-        git branch: 'main', url: "https://github.com/CodingSudeep/${COMPONENT}"
-        env.APP_TYPE == "nodejs"
-        
-        stage('Docker Build') {
-            sh "docker build ."
+        git branch: 'main', url: "https://github.com/b49-clouddevops/${COMPONENT}"
+        stage('Docker Build') {           
+            sh "docker build -t 213243212489.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
         }
-        // common.lintChecks()
-        // common.sonarCheck()
-        // common.testCases()
-        // if(env.TAG_NAME != null) {
-        // common.artifacts()
+
+        if(env.TAG_NAME != null) {
+        stage('Docker Push') {
+            sh "docker tag 213243212489.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 213243212489.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}" 
+            sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 213243212489.dkr.ecr.us-east-1.amazonaws.com"
+            sh "docker push 213243212489.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+            }
         }
     }
+}
